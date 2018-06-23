bf88eaae35a2c14d03b8ba561d188cc28096639c
scottnuma <scott.numamoto@gmail.com>
2018-06-22 22:04:05 -0700

    Remove extra paths

--- serve.py
+++ serve.py
@@ -3,7 +3,7 @@ import spotipy
 import spotipy.oauth2 as oauth2
 import analysis
 
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, send_from_directory
 
 assert 'CLIENT_ID' in os.environ
 CLIENT_ID = os.environ['CLIENT_ID']
@@ -16,15 +16,19 @@ REDIRECT_URI = os.environ['REDIRECT_URI']
 
 SCOPE = 'user-library-read user-top-read playlist-read-private playlist-read-collaborative'
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def hello_world():
     return 'hello'
 
+@app.route('/analysis')
+def analysis():
+    return send_from_directory(app.static_folder, 'index.html')
+
 @app.route('/static/<path:path>')
 def sender(path):
-    return send_from_directory('static', path)
+    return send_from_directory(app.static_folder, path)
 
 @app.route('/login/')
 def register():
@@ -34,20 +38,10 @@ def register():
             REDIRECT_URI,
             scope=SCOPE)
     auth_url = sp_oauth.get_authorize_url()
-    print("Redirecting to %s" % auth_url)
     return redirect(auth_url)
-
-@app.route('/okay')
-def okay():
-    return "more"
-
-@app.route('/alpha')
-def alpha():
-    return redirect("http://localhost:8000/static/index.html?username0=%s" % "newmascot")
 
 @app.route('/token')
 def receive_token():
-    print("Received token")
     code = request.args.get('code')
     sp_oauth = oauth2.SpotifyOAuth(
             CLIENT_ID,
@@ -58,7 +52,6 @@ def receive_token():
     token_info = sp_oauth.get_access_token(code)
     user = analysis.User(token_info['access_token'])
     user.record()
-    print("Recorded - redirecting to viewer")
     return redirect("http://localhost:8000/static/index.html?username0=%s" % user.username)
 
 if __name__ == "__main__":
