2edcdc5565cd006fbebbd35be4544190b7c56548
scottnuma <scott.numamoto@gmail.com>
2018-06-22 21:50:26 -0700

    - Save data to subdir - Focus user object on token rather than username - Move token retrieval to server

--- analysis.py
+++ analysis.py
@@ -5,36 +5,13 @@ import spotipy.util as util
 import numpy as np
 import json
 
-# Retrieve secrets.
-
-assert 'CLIENT_ID' in os.environ
-CLIENT_ID = os.environ['CLIENT_ID']
-
-assert 'CLIENT_SECRET' in os.environ
-CLIENT_SECRET = os.environ['CLIENT_SECRET']
-
-assert 'REDIRECT_URI' in os.environ
-REDIRECT_URI = os.environ['REDIRECT_URI']
-
-
-# Authenticate with Spotify API
-
 SCOPE = 'user-library-read user-top-read playlist-read-private playlist-read-collaborative'
 
 class User:
-    def __init__(this, username):
-        this.username = username
-
-        token = util.prompt_for_user_token(this.username,
-                                           SCOPE,
-                                           client_id=CLIENT_ID,
-                                           client_secret=CLIENT_SECRET,
-                                           redirect_uri=REDIRECT_URI)
-        if not token:
-            print("Unable to get token for", username)
-            throw -1
+    def __init__(this, token):
 
         sp = spotipy.Spotify(auth=token)
+        this.username = sp.current_user()['id']
         time_frames = ['short_term', 'medium_term', 'long_term']
         this.top_tracks = dict()
         for time_frame in time_frames:
@@ -50,8 +27,11 @@ class User:
                 separators=(',',': '))
 
     def record(this):
+        curr_dir = os.path.dirname(os.path.realpath(__file__))
+        subdir = os.path.join("static", "user_tracks")
         filename = this.username + ".json"
-        with open(filename, 'w') as f:
+        filepath = os.path.join(curr_dir, subdir, filename)
+        with open(filepath, 'w') as f:
             f.write(this.json())
 
 class Song:
@@ -70,20 +50,23 @@ def populate(sp, songs):
 
     features = sp.audio_features(org.keys())
     for feature_set in features:
-        song = org[feature_set['id']]
-        song.danceability = feature_set['danceability']
-        song.energy = feature_set['energy']
-        song.key = feature_set['key']
-        song.loudness = feature_set['loudness']
-        song.mode = feature_set['mode']
-        song.speechiness = feature_set['speechiness']
-        song.acousticness = feature_set['acousticness']
-        song.instrumentalness = feature_set['instrumentalness']
-        song.liveness = feature_set['liveness']
-        song.valence = feature_set['valence']
-        song.tempo = feature_set['tempo']
-        song.duration_ms = feature_set['duration_ms']
-        song.time_signature = feature_set['time_signature']
+        if feature_set:
+            song = org[feature_set['id']]
+            song.danceability = feature_set['danceability']
+            song.energy = feature_set['energy']
+            song.key = feature_set['key']
+            song.loudness = feature_set['loudness']
+            song.mode = feature_set['mode']
+            song.speechiness = feature_set['speechiness']
+            song.acousticness = feature_set['acousticness']
+            song.instrumentalness = feature_set['instrumentalness']
+            song.liveness = feature_set['liveness']
+            song.valence = feature_set['valence']
+            song.tempo = feature_set['tempo']
+            song.duration_ms = feature_set['duration_ms']
+            song.time_signature = feature_set['time_signature']
+        else:
+            print(feature_set, features, org)
 
 if __name__ == "__main__":
     user = User("newmascot")
